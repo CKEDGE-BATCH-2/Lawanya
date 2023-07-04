b66473053bf51d8eabe5c845c173b2e1dc6e51bc
Lawanyashiva <137751812+Lawanyashiva@users.noreply.github.com>
2023-07-04 10:42:12 +0530

    php 1-task
    I Have attached my php task 1...

---
+++ php task 1/max.php
@@ -0,0 +1,6 @@
+<?php
+$number=array('1','2','3','4','5','6','7');
+echo (max($number));
+echo("<br>");
+echo(min($number));
+?>
---
+++ php task 1/size.php
@@ -0,0 +1,6 @@
+<?php
+    $fruits=array('apple','papaya','mango','Watermelon','grapes');
+    echo count($fruits);
+    echo"<br>";
+    echo sizeof($fruits);
+?>
